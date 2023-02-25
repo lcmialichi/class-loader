54ca1847700f34f3ca06713d0c876de50def1c19
Lucas mialichi <lmcmialichi@gmail.com>
2023-02-25 11:01:25 -0300

    add config and strucutre

---
+++ tests/load.php
@@ -0,0 +1,9 @@
+<?php
+
+use ClassLoad\Loader;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+
+$loader = new Loader(ClassLoad::class, true);
+$loader->config(["app-root" => realpath(__DIR__ . "/..")]);
+var_dump($loader->load());
